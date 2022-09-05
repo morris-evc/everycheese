63ebdf051bd8e3d745defb57ca6d3eb20ce4f984
Evan Morris <evc.morris@gmail.com>
2022-09-05 16:35:38 -0400

    Add bio field to user model, views, templates

--- everycheese/users/models.py
+++ everycheese/users/models.py
@@ -12,6 +12,9 @@ class User(AbstractUser):
         _("Name of User"), blank=True, max_length=255
     )
 
+    #adding bio field for user bios
+    bio = models.TextField("Bio", blank=True)
+
     def get_absolute_url(self):
         return reverse(
             "users:detail", kwargs={"username": self.username}
--- everycheese/users/views.py
+++ everycheese/users/views.py
@@ -24,6 +24,8 @@ user_detail_view = UserDetailView.as_view()
 class UserUpdateView(LoginRequiredMixin, UpdateView):
     fields = [
         "name",
+        #adding bio to list
+        "bio",
     ]
 
     # We already imported user in the View code above,
